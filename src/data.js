7da83bbb823b729d0dce3e27cb25bd4340338353
Ashish Singh <ashishsingh.1899@gmail.com>
2022-07-17 14:26:10 +0530

    update

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1519865885898-a54a6f2c7eea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3BsYXNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1498462440456-0dba182e775b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3BsYXNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1459802071246-377c0346da93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c3BsYXNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c3BsYXNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1594897030264-ab7d87efc473?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c3BsYXNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1587473555771-96aef0d968cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNwbGFzaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60",
+    link: "http://lama.dev",
+  },
+];
